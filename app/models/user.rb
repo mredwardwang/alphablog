645e9aecbd019d60b2acb81e7f5fe43cc0ab0c16
Ed Wang <mredwardwang@gmail.com>
2020-04-04 16:42:40 -0700

    add association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :articles
+    before_save { self.email = email.downcase }
     validates   :username, presence: true, 
                 uniqueness: { case_sensitive: false }, 
                 length: { minimum: 3, maximum: 25 }
@@ -9,4 +11,6 @@ class User < ApplicationRecord
                 uniqueness: { case_sensitive: false },
                 length: { maximum: 105 },
                 format: { with: VALID_EMAIL_REGEX }
+
+        
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_205132) do
+ActiveRecord::Schema.define(version: 2020_04_04_214706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_04_205132) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
