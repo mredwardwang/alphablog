2df60eb5a6d5538cc465747f1e9c1cc5c0150c89
Ed Wang <mredwardwang@gmail.com>
2020-04-04 19:01:34 -0700

    add signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
   resources :articles
+
+
 end
