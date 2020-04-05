7b78d3318f443d46283e4639508d0758bf30cc0c
Ed Wang <mredwardwang@gmail.com>
2020-04-04 21:01:06 -0700

    add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   
   root 'pages#home'
   get 'about', to: 'pages#about'
+
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
 
   resources :articles
-
 
 end
